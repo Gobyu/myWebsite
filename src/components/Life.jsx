7bab62fd625e225b65948ccf9afd453fbf8e0282
John Zhao <john.zhao166@gmail.com>
2024-01-22 21:02:56 -0500

    update website

--- src/components/Life.jsx
+++ src/components/Life.jsx
@@ -16,12 +16,14 @@ const Life = ({ CSSTheme }) => {
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/cat_plushie.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">🐱 🐱 🐱</p>
           </div>
           <div className="img-container">
             <img
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/coco_art.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">Can someone translate this?</p>
           </div>
         </div>
         <div className="columns">
@@ -30,18 +32,21 @@ const Life = ({ CSSTheme }) => {
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/maoi.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">🗿 🗿 🗿</p>
           </div>
           <div className="img-container">
             <img
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/monkey.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">🖕</p>
           </div>
           <div className="img-container">
             <img
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/neemo.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">Where's my fuckin son</p>
           </div>
         </div>
         <div className="columns">
@@ -62,6 +67,7 @@ const Life = ({ CSSTheme }) => {
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/stop.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">Stop it, get some help.</p>
           </div>
         </div>
         <div className="columns">
@@ -70,18 +76,23 @@ const Life = ({ CSSTheme }) => {
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/we_the_north.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">
+              The Toronto Raptors are your 2024 NBA Champions.
+            </p>
           </div>
           <div className="img-container">
             <img
               src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/chatime_stacking.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">2023 Chatime Olympics</p>
           </div>
           <div className="img-container">
             <img
-              src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/stop.jpg?raw=true"
+              src="https://github.com/Gobyu/Gobyu.github.io/blob/main/assets/adopted.jpg?raw=true"
               className="life-image"
             />
+            <p className="img-overlay">:&#40;</p>
           </div>
         </div>
       </div>
